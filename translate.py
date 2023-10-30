824af9b44766f1b7b35817b0b838d35b70d68633
diversen <dennis.iversen@gmail.com>
2023-10-30 21:15:06 +0100

    update example

--- translate.py
+++ translate.py
@@ -4,20 +4,8 @@ import logging
 # set logging level
 logging.basicConfig(level=logging.INFO)
 
-pre_prompt = """You WILL modernize an H.P. lovecraft text to a more modern and easy readable English.
-Words that are outdated or not very common anymore should be replaced with words that convey the same meaning.
-You MUST not use any meta-comments.
-You MUST keep any markdown formatting intact when possible, e.g. markdown header like '#' or '##'.
-The text may be a headline or a full paragraph. You MUST translate from English to a more Modern English.
-The text to modernize begins after the next colon: """
-
-# pre_prompt = """
-# You are an agent that is fluent in both English and Danish.
-# You should internally translate to a modern English first. And then translate this modern English to Danish.
-# You MUST not use any meta-comments.
-# You MUST keep any markdown formatting intact when possible, e.g. markdown header like '#' or '##'.
-# The text may be a headline or a full paragraph.
-# The text to translate to Danish begins after the next colon: """
+pre_prompt = """Translate the following H. P. Lovecraft text to English so that I child aged 12 will have no 
+problem reading it. Add some humor to the text if possible. """
 
 # Initialize the GPTTranslate class and call the translate method
 gpt_translate = GPTTranslator(
