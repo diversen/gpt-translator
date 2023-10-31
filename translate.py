dc32e27d9f529947addeae6d6356d171599a21da
diversen <dennis.iversen@gmail.com>
2023-10-31 07:28:57 +0100

    fix example

--- translate.py
+++ translate.py
@@ -4,15 +4,14 @@ import logging
 # set logging level
 logging.basicConfig(level=logging.INFO)
 
-pre_prompt = """Translate the following two scenes from Hamlet by Shakespeare 
+prompt = """Translate the following two scenes from Hamlet by Shakespeare 
 to a modern version so that it is easier to understand. It should be as simple as possible, but no simpler. """
 
 # Initialize the GPTTranslate class and call the translate method
 gpt_translate = GPTTranslator(
     "./output/hamlet_part.md",
-    pre_prompt,
+    prompt,
     working_dir="./output",
-    idx_begin=0,
     model="gpt-3.5-turbo",
     max_tokens_paragraph=1024,
 )
